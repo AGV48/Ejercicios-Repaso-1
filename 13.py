b375f0dca32964624f056715345accc2ce930217
Alejandro Gutierrez <agutierrez289@soyudemedellin.edu.co>
2023-08-03 16:17:27 -0500

    Punto 13

---
+++ 13.py
@@ -0,0 +1,20 @@
+Num1 = int(input("Ingrese el primer numero para operar: "))
+Num2 = int(input("Ingrese el segundo numero para operar: "))
+
+print("")
+
+print("La suma de los dos numeros es: ", Num1+Num2)
+
+print("")
+
+print(f"La resta de {Num1} - {Num2} es: {Num1 - Num2}")
+print(f"La resta de {Num2} - {Num1} es: {Num2 - Num1}")
+
+print("")
+
+print("La multiplicación de los dos numeros es: ", Num1*Num2)
+
+print("")
+
+print(f"La división de {Num1} dividido {Num2} es: {Num1/Num2}")
+print(f"La división de {Num2} dividido {Num1} es: {Num2/Num1}")
